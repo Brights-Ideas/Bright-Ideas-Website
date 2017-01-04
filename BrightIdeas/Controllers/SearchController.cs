b5964890dc46761daff61d8176b8b4e0b516b97f
Andrew Bright <abright2012@gmail.com>
2017-01-04 00:09:27 +0000

    Search mvc implementation

--- BrightIdeas/Controllers/SearchController.cs
+++ BrightIdeas/Controllers/SearchController.cs
@@ -1,4 +1,5 @@
-﻿using Examine;
+﻿using BrightIdeas.Models;
+using Examine;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,33 +18,74 @@ namespace BrightIdeas.Controllers
             //ExamineManager.Instance.IndexProviderCollection[Helpers]
         }
         // GET: Search
-        public ActionResult Index()
+        [HttpGet]
+        public ActionResult Search()
         {
-            return View();
+            return PartialView("_SearchFormPartial");
         }
 
+        [HttpPost]
+        public ActionResult Search(string query)
+        {
+            if (query != null)
+            {
+                try
+                {
+                    int parsedInt;
+                    //var searchlist = rep.Search(query);
+
+                    var model = new SearchViewModel()
+                    {
+                        //NewsList = new List<NewsViewModel>()
+                        //SearchTerms = new Dictionary<string, string>(),
+                        Location = "" + Request["Location"],//.ToLower(CultureInfo.InvariantCulture),
+                        MinPrice = Convert.ToInt32(Request["MinPrice"]),
+                        MaxPrice = Convert.ToInt32(Request["MaxPrice"]),
+
+                        CurrentPage = int.TryParse(Request["p"], out parsedInt) ? parsedInt : 1,
+
+                        PageSize = 9,
+                        RootContentNodeId = 1402,
+                        //RootMediaNodeId = GetMacroParam(Model, "rootMediaNodeId", s => int.Parse(s), -1),
+                        //IndexType = GetMacroParam(Model, "indexType", s => s.ToLower(CultureInfo.InvariantCulture), ""),
+                        //SearchFields = GetMacroParam(Model, "searchFields", s => SplitToList(s), new List<string> { "nodeName", "metaTitle", "metaDescription", "metaKeywords", "bodyText" }),
+                        //PreviewFields = GetMacroParam(Model, "previewFields", s => SplitToList(s), new List<string> { "bodyText" }),
+                        PreviewLength = 250,
+                        //HideFromSearchField = GetMacroParam(Model, "hideFromSearchField", "umbracoNaviHide"),
+                        //SearchFormLocation = "top"
+                    };
+
+                    return PartialView("_SearchResultsPartial", model);
+                }
+                catch (Exception ex)
+                {
+                    // handle exception
+                }
+            }
+            return PartialView("Error");
+        }
         //public ActionResult Index(string sortOrder)
         //{
         //    ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
         //    ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
-            //var students = from s in db.Students
-                           //select s;
-            //switch (sortOrder)
-            //{
-            //    case "name_desc":
-            //        students = students.OrderByDescending(s => s.LastName);
-            //        break;
-            //    case "Date":
-            //        students = students.OrderBy(s => s.EnrollmentDate);
-            //        break;
-            //    case "date_desc":
-            //        students = students.OrderByDescending(s => s.EnrollmentDate);
-            //        break;
-            //    default:
-            //        students = students.OrderBy(s => s.LastName);
-            //        break;
-            //}
-            //return View(students.ToList());
+        //var students = from s in db.Students
+        //select s;
+        //switch (sortOrder)
+        //{
+        //    case "name_desc":
+        //        students = students.OrderByDescending(s => s.LastName);
+        //        break;
+        //    case "Date":
+        //        students = students.OrderBy(s => s.EnrollmentDate);
+        //        break;
+        //    case "date_desc":
+        //        students = students.OrderByDescending(s => s.EnrollmentDate);
+        //        break;
+        //    default:
+        //        students = students.OrderBy(s => s.LastName);
+        //        break;
+        //}
+        //return View(students.ToList());
         //}
     }
 }
---
+++ BrightIdeas/Models/SearchViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Examine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BrightIdeas.Models
+{
+    public class SearchViewModel
+    {
+        // Query Parameters
+
+        public int MinPrice { get; set; }
+        public int MaxPrice { get; set; }
+        public string Location { get; set; }
+        public string SearchTerm { get; set; }
+        //public Dictionary<string, string> SearchTerms { get; set; }
+        public int CurrentPage { get; set; }
+
+        // Options
+        public int RootContentNodeId { get; set; }
+        public int RootMediaNodeId { get; set; }
+        public string IndexType { get; set; }
+
+        public int PreviewLength { get; set; }
+        public int PageSize { get; set; }
+
+        // Results
+        public int TotalResults { get; set; }
+        public int TotalPages { get; set; }
+
+        public IEnumerable<SearchResult> AllResults { get; set; }
+        public IEnumerable<SearchResult> PagedResults { get; set; }
+    }
+}
